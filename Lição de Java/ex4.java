9dfb9c76e3d33bf036d68a474cd69bd02bc03f00
Pedro Alves <125505087+PEDRO160126@users.noreply.github.com>
2024-08-26 11:27:20 -0300

    Update ex4.java

--- Lição de Java/ex4.java
+++ Lição de Java/ex4.java
@@ -1,10 +1,17 @@
 //Peça o raio de um círculo, calcule e mostre sua área.
 
 import java.util.Scanner;
-public class Circulo{
+public class ex4{
     public static void main(String[] args){
-        Scanner = leitor.nextInt();
-        Scanner leitor = new (Scanner.in);
-        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o valor do raio do circulo: ");
+        int raio = leitor.nextInt();
+
+        double area = 3.14 * (raio * raio);
+
+        System.out.println("A área do circulo é:" + area);
+
+        leitor.close();
     }
 }
