81b8dc266de844999907b1fe196803919280026e
Pedro Alves <125505087+PEDRO160126@users.noreply.github.com>
2024-08-26 11:26:25 -0300

    Update ex3.java

--- Lição de Java/ex3.java
+++ Lição de Java/ex3.java
@@ -3,15 +3,15 @@
 import java.util.Scanner;
 public class Conversor{
     public static void main(String[] args){
-        Scanner leitor = new (Scanner.in);
+        Scanner leitor = new Scanner(System.in);
 
         System.out.println("Digite o valor em métros:" );
         int valor = leitor.nextInt();
 
-        int converter = valor / 100;
+        int converter = valor * 100;
 
         System.out.println("O valor de" + valor + "em centimetros é de :" + converter);
 
-        leitor.close()
+        leitor.close();
     }
 }
